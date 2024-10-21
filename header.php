44fe5bdc78f8d1a7ac65c76551b2fd2afaef35db
sandymoh <sandymohammad99@gmail.com>
2024-10-21 08:19:03 +0700

    feat: update idc header

--- header.php
+++ header.php
@@ -3,7 +3,12 @@
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link
+      href="https://fonts.googleapis.com/css2?family=Nunito:wght@100;200;300;400;500;600;700;800;900&display=swap"
+      rel="stylesheet"
+    />
     <?php wp_head(); ?>
+    
   </head>
   <body>
     
