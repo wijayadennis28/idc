b146cdbb52a1696d9c7bfd6535d84b80c1358a2c
sandymoh <sandymohammad99@gmail.com>
2024-10-29 20:32:09 +0700

    feat: create loading component

---
+++ src/scripts/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex h-[60vh] items-center justify-center">
+      <span className="loading loading-bars loading-lg text-primary"></span>{" "}
+    </div>
+  );
+};
+
+export default Loading;
