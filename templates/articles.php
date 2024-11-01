fbbdb2aedf1e78f55d8b99378d94857a21d761b9
sandymoh <sandymohammad99@gmail.com>
2024-11-01 20:56:24 +0700

    feat: add new article template with header and footer sections

---
+++ templates/articles.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: Article Template
+*/
+?>
+
+<?php
+
+get_header(); ?>
+
+<header id="header"></header>
+
+<div>
+
+  <!-- example react component -->
+  <div id="article"></div>
+  <!-- end example react component -->
+
+  
+</div>
+
+<div id="footer"></div>
+
+
+<?php get_footer();
