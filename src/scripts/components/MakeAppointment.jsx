c099cc811419aff7a6ff035278340b84c95e836a
sandymoh <sandymohammad99@gmail.com>
2024-11-03 15:20:07 +0700

    feat: enhance layout and styling of Make Appointment component for improved responsiveness and visual appeal

--- src/scripts/components/MakeAppointment.jsx
+++ src/scripts/components/MakeAppointment.jsx
@@ -6,30 +6,37 @@ import logoWithoutTextBig from "../../../assets/image/logo/logo-without-text-big
 
 const MakeAppointment = () => {
   return (
-    <div className="m-8" id="make-appointment">
-      <div className="flex h-44">
-        <div className="flex-1"></div>
-        <div className="flex flex-1 justify-center">
-          <div className="absolute">
-            <img src={logoWithoutTextBig} alt="logo" style={{ width: 439 }} />
+    <div className="flex justify-center">
+      <div className="w-full max-w-[1643px] p-4 md:p-8" id="make-appointment">
+        <div className="hidden h-28 md:flex lg:h-44">
+          <div className="flex-1"></div>
+          <div className="flex flex-1 justify-center">
+            <div className="absolute">
+              <img
+                src={logoWithoutTextBig}
+                alt="logo"
+                className="size-[380px] lg:size-[439px]"
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex rounded-2xl bg-[color:#E0EFF3] p-16">
-        <div className="flex flex-1 flex-col gap-6">
-          <h1 className="text-primary">
-            <span className="font-normal">Ready to transform</span> your smile
-          </h1>
-          <p>
-            Your journey to a confident, beautiful smile starts here.
-            <br />
-            Let's make it happen together!
-          </p>
-          <button className="btn btn-primary w-fit">
-            Make an Appointment today! <ArrowLongRightIcon className="size-5" />
-          </button>
+        <div className="flex rounded-2xl bg-[color:#E0EFF3] px-2 py-16 text-center md:px-8 md:py-8 md:text-left lg:p-16">
+          <div className="flex flex-1 flex-col items-center gap-6 md:items-start">
+            <h1 className="text-primary">
+              <span className="font-normal">Ready to transform</span> your smile
+            </h1>
+            <p>
+              Your journey to a confident, beautiful smile starts here.
+              <br />
+              Let's make it happen together!
+            </p>
+            <button className="btn btn-primary w-fit">
+              Make an Appointment today!{" "}
+              <ArrowLongRightIcon className="size-5" />
+            </button>
+          </div>
+          <div className="hidden flex-1 md:flex"></div>
         </div>
-        <div className="flex-1"></div>
       </div>
     </div>
   );
