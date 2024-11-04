70af50fb6293257d587a6ae02103400401feffaf
sandymoh <sandymohammad99@gmail.com>
2024-11-04 23:22:56 +0700

    feat: update PopUpMap component layout and import Branches

--- src/scripts/components/PopUpMap.jsx
+++ src/scripts/components/PopUpMap.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import Branches from "./Branches";
+
 const PopUpMap = () => {
   return (
     <dialog id="map-modal" className="modal">
-      <div className="modal-box">
+      <div className="modal-box w-11/12 max-w-5xl">
         <form method="dialog">
           {/* if there is a button in form, it will close the modal */}
           <button className="btn btn-circle btn-primary btn-sm absolute right-2 top-2">
