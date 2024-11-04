2511ba113e2e88023c877d657f78730eba238884
sandymoh <sandymohammad99@gmail.com>
2024-11-04 23:23:04 +0700

    fix: improve text formatting in AboutUs component

--- src/scripts/aboutus.jsx
+++ src/scripts/aboutus.jsx
@@ -38,10 +38,13 @@ const AboutUs = () => {
       {/* Our Works */}
       <section className="bg-white px-4 py-16 md:px-16 lg:px-24">
         <h2 className="mb-8 text-center text-4xl font-bold text-purple-900">
-        Our Complete Clinic Equipment
+          Our Complete Clinic Equipment
         </h2>
         <p className="mx-auto mb-16 max-w-2xl text-center text-gray-700">
-        At Indo Dental Center, we believe that exceptional care begins with exceptional tools. Our clinic is equipped with the latest, cutting-edge technology to ensure you receive the best possible treatment in a comfortable and efficient manner.
+          At Indo Dental Center, we believe that exceptional care begins with
+          exceptional tools. Our clinic is equipped with the latest,
+          cutting-edge technology to ensure you receive the best possible
+          treatment in a comfortable and efficient manner.
         </p>
       </section>
       <ClinicEquipment />
