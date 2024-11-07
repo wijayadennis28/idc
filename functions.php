6babb0840d551a875ce4afb057b8b175223d7adc
Irfaan Dzakiy <dz4k1y@gmail.com>
2024-11-07 22:30:14 +0700

    feat: add article tags to services

--- functions.php
+++ functions.php
@@ -24,3 +24,31 @@ function register_my_menus() {
   );
 }
 add_action( 'init', 'register_my_menus' );
+
+function add_tags_string_to_rest_api() {
+  register_rest_field( 'services', 
+      'article-tags', 
+      array(
+          'get_callback' => 'get_tags_as_string',
+          'schema'       => null,
+      )
+  );
+}
+
+function get_tags_as_string( $object ) {
+  $post_id = $object['id'];
+
+  $tags = wp_get_post_terms( $post_id, 'article-tags' ); 
+
+  if ( ! empty( $tags ) && ! is_wp_error( $tags ) ) {
+    $tag_names = wp_list_pluck( $tags, 'name' );
+    $tag_names = array_map( function( $name ) {
+      return '#' . $name;
+  }, $tag_names );
+    return implode(' ', $tag_names);
+  }
+
+  return ''; 
+}
+
+add_action( 'rest_api_init', 'add_tags_string_to_rest_api' );
