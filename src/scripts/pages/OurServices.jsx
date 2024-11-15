4c5398301314e3034555c8901291c9c7bd50001e
sandymoh <sandymohammad99@gmail.com>
2024-11-15 11:33:38 +0700

    feat: center align content in OurServices component

--- src/scripts/pages/OurServices.jsx
+++ src/scripts/pages/OurServices.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import OurClinicImg from "../../../assets/image/our-services/our-clinic.jpg";
 import Departments from "../components/Departments";
-import MakeAppointment from '../components/MakeAppointment';
+import MakeAppointment from "../components/MakeAppointment";
 
 const OurServices = () => {
   return (
-    <div className="flex flex-col">
-      <div
-        style={{
-          '--image-url': `url(${OurClinicImg})`,
-        }}
-        className={String.raw`w-full h-72 bg-cover bg-center bg-[image:var(--image-url)]`}
-      >
-        <div className="flex justify-center items-center bg-primary bg-opacity-75 h-full">
-          <h1 className="font-sans text-white text-center font-normal">We provide the <span className="font-bold italic">best service</span></h1>
+    <>
+      <div className="flex flex-col items-center">
+        <div
+          style={{
+            "--image-url": `url(${OurClinicImg})`,
+          }}
+          className={String.raw`h-72 w-full bg-[image:var(--image-url)] bg-cover bg-center`}
+        >
+          <div className="flex h-full items-center justify-center bg-primary bg-opacity-75">
+            <h1 className="text-center font-sans font-normal text-white">
+              We provide the{" "}
+              <span className="font-bold italic">best service</span>
+            </h1>
+          </div>
+        </div>
+        <div className="pb-24">
+          <Departments />
         </div>
       </div>
-      <div className="pb-24">
-        <Departments/>
-      </div>
-      <MakeAppointment/>
-    </div>
-  )
+      <MakeAppointment />
+    </>
+  );
 };
 
 export default OurServices;
