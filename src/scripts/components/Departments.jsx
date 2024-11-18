8ba52b8c60bca886e2fcb40b7b09becd9efdc6a9
sandymoh <sandymohammad99@gmail.com>
2024-11-18 11:52:36 +0700

    feat: update department card

--- src/scripts/components/Departments.jsx
+++ src/scripts/components/Departments.jsx
@@ -26,49 +26,41 @@ const Departments = () => {
     <div className="mt-8 flex max-w-[2292px] flex-wrap justify-stretch gap-12 max-lg:gap-4 max-lg:px-4 max-md:flex-col max-md:gap-8 max-md:px-4 md:justify-center">
       {departments.map((department, index) => (
         <a href={department.link} key={index}>
-          <div
-            style={{
-              backgroundImage: `url(${department.meta?.image})`,
-            }}
-            className="group card h-full w-[100%] cursor-pointer rounded-2xl bg-[color:#fcfbfd] bg-[length:40%] bg-right-top bg-no-repeat hover:bg-primary hover:!bg-none md:w-[360px] 2xl:w-[420px]"
-          >
-            <div className="h-full w-full rounded-2xl bg-gradient-to-bl from-white/50 to-white to-40% group-hover:!bg-none">
-              <div className="card-body">
-                <div className="mb-8 flex justify-between">
-                  <img src={department.meta?.icon} />
-                  <div className="card-actions">
-                    <button className="btn btn-secondary no-animation hidden text-base font-normal group-hover:block">
-                      See details
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        className="ml-2 inline size-5 text-secondary"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-                <h4
-                  className="text-primary group-hover:text-white"
-                  dangerouslySetInnerHTML={{
-                    __html: department.title.rendered,
-                  }}
-                ></h4>
-                <p
-                  className="font-normal text-[#4D4757] group-hover:text-white"
-                  dangerouslySetInnerHTML={{
-                    __html: department.meta?.card_text,
-                  }}
-                ></p>
+          <div className="group card relative h-56 w-[421px] overflow-hidden !bg-white bg-right bg-no-repeat text-primary-content hover:!bg-primary hover:!bg-none">
+            <div className="card-body z-10 flex-row justify-between">
+              <h4
+                className="w-[168px] overflow-visible text-primary group-hover:text-white"
+                dangerouslySetInnerHTML={{
+                  __html: department.title.rendered,
+                }}
+              ></h4>
+              <div className="card-actions hidden group-hover:block">
+                <button className="btn btn-secondary no-animation text-base font-normal">
+                  See details
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="ml-2 inline size-5 text-secondary"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                    />
+                  </svg>
+                </button>
               </div>
+            </div>
+            <div className="absolute -right-36 group-hover:hidden">
+              <img
+                src={department.meta?.image}
+                alt={department.title.rendered + " image"}
+                className="opacity-50"
+              />
+              <div class="absolute inset-0 w-10 bg-gradient-to-r from-white to-transparent"></div>
             </div>
           </div>
         </a>
