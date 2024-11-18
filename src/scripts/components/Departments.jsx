05764a1beff1e475196d8e15d9a84fdc0998b83b
sandymoh <sandymohammad99@gmail.com>
2024-11-18 12:16:45 +0700

    feat: enhance department card layout and add icon

--- src/scripts/components/Departments.jsx
+++ src/scripts/components/Departments.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 
 import Loading from "./Loading";
 
+import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
+
 const Departments = () => {
   const [departments, setDepartments] = useState([]);
 
@@ -23,10 +25,10 @@ const Departments = () => {
   if (departments.length === 0) return <Loading />;
 
   return (
-    <div className="mt-8 flex max-w-[2292px] flex-wrap justify-stretch gap-12 max-lg:gap-4 max-lg:px-4 max-md:flex-col max-md:gap-8 max-md:px-4 md:justify-center">
+    <div className="mt-8 flex max-w-[2292px] flex-wrap justify-stretch gap-8 max-lg:gap-4 max-lg:px-4 max-md:flex-col max-md:gap-8 max-md:px-4 md:justify-center">
       {departments.map((department, index) => (
         <a href={department.link} key={index}>
-          <div className="group card relative h-56 w-[421px] overflow-hidden !bg-white bg-right bg-no-repeat text-primary-content hover:!bg-primary hover:!bg-none">
+          <div className="group card relative h-48 w-[380px] overflow-hidden !bg-white bg-right bg-no-repeat text-primary-content hover:!bg-primary hover:!bg-none">
             <div className="card-body z-10 flex-row justify-between">
               <h4
                 className="w-[168px] overflow-visible text-primary group-hover:text-white"
@@ -35,29 +37,16 @@ const Departments = () => {
                 }}
               ></h4>
               <div className="card-actions hidden group-hover:block">
-                <button className="btn btn-secondary no-animation text-base font-normal">
+                <button className="btn btn-secondary no-animation btn-sm h-11 w-fit">
                   See details
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    className="ml-2 inline size-5 text-secondary"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                    />
-                  </svg>
+                  <ArrowUpRightIcon className="w-4" />
                 </button>
               </div>
             </div>
             <div className="absolute -right-36 group-hover:hidden">
               <img
                 src={department.meta?.image}
-                alt={department.title.rendered + " image"}
+                alt={department.title?.rendered + "image"}
                 className="opacity-50"
               />
               <div class="absolute inset-0 w-10 bg-gradient-to-r from-white to-transparent"></div>
