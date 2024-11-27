e8d82865451df56a54030579a798a8d3a5069a29
sandymoh <sandymohammad99@gmail.com>
2024-11-27 16:08:51 +0700

    feat: replace department card implementation with IdcCard component for improved structure and reusability

--- src/scripts/components/Departments.jsx
+++ src/scripts/components/Departments.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import Loading from "./Loading";
 
-import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
+import IdcCard from "./IdcCard";
 
 const Departments = ({ departments = [] }) => {
   if (departments.length === 0) return <Loading />;
@@ -11,35 +11,11 @@ const Departments = ({ departments = [] }) => {
     <div className="mt-8 flex max-w-[2292px] flex-wrap justify-stretch gap-8 max-lg:gap-4 max-lg:px-4 max-md:flex-col max-md:gap-8 max-md:px-4 max-sm:px-0 md:justify-center">
       {departments.map((department, index) => (
         <a href={department.link} key={index}>
-          <div className="group card relative h-44 w-full overflow-hidden !bg-white bg-right bg-no-repeat text-primary-content shadow-sm hover:!bg-primary hover:!bg-none sm:w-[340px] lg:h-48 lg:w-[380px]">
-            <div className="group card relative h-full w-full overflow-hidden">
-              {/* Image and gradient */}
-              <div className="absolute inset-0 group-hover:hidden">
-                <img
-                  src={department.meta?.image}
-                  alt={`${department.title?.rendered} image`}
-                  className="h-full w-full object-cover opacity-50"
-                />
-                <div className="absolute inset-0 w-10 bg-gradient-to-r from-white to-transparent"></div>
-              </div>
-
-              {/* Card content */}
-              <div className="card-body relative z-10 flex flex-row flex-wrap justify-between">
-                <h4
-                  className="w-[138px] overflow-visible text-primary group-hover:text-white lg:w-[168px]"
-                  dangerouslySetInnerHTML={{
-                    __html: department.title.rendered,
-                  }}
-                ></h4>
-                <div className="card-actions hidden group-hover:block">
-                  <button className="btn btn-secondary no-animation btn-sm h-11 w-fit">
-                    See details
-                    <ArrowUpRightIcon className="w-4" />
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
+          <IdcCard
+            image={department.meta?.image}
+            title={department.title.rendered}
+            hover={true}
+          />
         </a>
       ))}
     </div>
