e0a062b28a6e0179dd8eb743323ab3add7cc7b87
sandymoh <sandymohammad99@gmail.com>
2024-12-19 20:11:02 +0700

    feat: detect default language from URL for localization

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -4,12 +4,17 @@ import { initReactI18next } from "react-i18next";
 import enTranslations from "./en.json";
 import idTranslations from "./id.json";
 
+const detectLanguageFromUrl = () => {
+  // check if wpApiSettings.homeUrl end with id or no id
+  return wpApiSettings.homeUrl.endsWith("/id/") ? "id" : "en";
+};
+
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: enTranslations },
     id: { translation: idTranslations },
   },
-  lng: "en", // Default language
+  lng: detectLanguageFromUrl(), // Default language
   fallbackLng: "en",
   interpolation: {
     escapeValue: false, // React already escapes values
